711306ec5a3a1261bf316e353128547907530636
Luke Eastman <LukeAEastman@gmail.com>
2020-09-11 16:36:30 -0700

    parse cookies

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,15 @@
 const parseCookies = (req, res, next) => {
+  let outputObj = {};
+  if (req.headers.cookie) {
+    var cookiesArr = req.headers.cookie.split('; ');
+
+    for (let cookie of cookiesArr) {
+      let splitCookie = cookie.split('=');
+      outputObj[splitCookie[0]] = splitCookie[1];
+    }
+  }
+  req.cookies = outputObj;
+  next();
 };
 
 module.exports = parseCookies;

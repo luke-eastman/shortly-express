dba11c2128857616c2a93fccb976fcb042cd018c
Luke Eastman <LukeAEastman@gmail.com>
2020-09-11 17:20:39 -0700

    add cookie to response

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,15 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  return models.Sessions.create()
+    .then(session => {
+      return models.Sessions.get({id: session.insertId});
+    })
+    .then(session => {
+      req.session = session;
+      res.cookie('shortlyid', session.hash);
+      next();
+    });
 };
 
 /************************************************************/
